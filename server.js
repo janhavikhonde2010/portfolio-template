64f503eea47c11e6164bad6ef7ca46ba0c7b4acf
Janhavi Khonde <janhavikhonde20@gmail.com>
2025-01-22 14:59:12 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Import routes
-const mainRoutes = require('./routes/mainRoutes');
-app.use('/', mainRoutes);
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
